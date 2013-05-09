c359a3a5944016d409cc3349fbf63cab824fd12e
GRR <guy.robinson@windowslive.com>
2013-05-10 00:03:51 +1200

    Init repo, working show/hide pane

---
+++ DockableUITest/Redbolts.DockableUITest/src/Redbolts.DockableUITest/UI/DockPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Redbolts.DockableUITest.UI
+{
+    /// <summary>
+    /// Interaction logic for DockPage.xaml
+    /// </summary>
+    public partial class DockPage 
+    {
+        public DockPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
